0c49ec6c8aaecfb51a2bce96401f3d9f96ca4c0f
Chandru <chandru.khemani@gmail.com>
2022-03-08 14:11:44 -0500

    update the method

--- GuessNumber/src/main/java/Main.java
+++ GuessNumber/src/main/java/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    // Declare a Scanner Object
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        // Start Game
+        startGame();
+
+        //Finish Game
+        System.out.println("\n Thank you for Playing");
+    }
+
+    //putting methods here
+    public static void startGame(){
+        System.out.println(getUserName());
+        //Game Logic
+    }
+    // Getting User Name
+    public static String getUserName() {
+        System.out.println("Please enter your Name;");
+        String name = scanner.next();
+        return "Welcome " + name + " to the guess the number game!" ;
     }
 }
